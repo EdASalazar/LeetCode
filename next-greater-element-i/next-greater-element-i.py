dd6c4dd47ceb7f0631bdc1127ea77387d2d7dde6
Edward Salazar <edasalazar123@gmail.com>
2023-05-02 19:26:36 -0700

    Time: 426 ms, Memory: 16.6 MB - LeetHub

---
+++ next-greater-element-i/next-greater-element-i.py
@@ -0,0 +1,26 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        ans = []
+        j = 0
+        k = 1
+        
+        for i in range(len(nums1)):
+            for j in range(len(nums2)):
+                pointer, curr = 1, 0
+                if nums1[i] == nums2[j]:
+                    curr = nums2[j]
+                    
+                    for k in range(j+1, len(nums2)):
+                        if nums2[k] > curr:
+                            ans.append(nums2[k])
+                            pointer = 0
+                            break
+                
+                    if pointer == 1:
+                        print(ans)
+                        ans.append(-1)
+                
+            
+          
+        
+        return ans
