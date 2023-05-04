1c2d2e18f8ecccc12c3a838ec3a72cb954915357
Edward Salazar <edasalazar123@gmail.com>
2023-05-04 10:39:49 -0700

    Time: 382 ms, Memory: 21.4 MB - LeetHub

--- online-stock-span/online-stock-span.py
+++ online-stock-span/online-stock-span.py
@@ -10,10 +10,8 @@ class StockSpanner:
         ans = 1
         while self.stack and self.stack[-1][0] <= price:
             ans += self.stack.pop()[1]
-            
         self.stack.append([price, ans])
         
-        print(self.stack)
         return ans
 
 
