ea32d2587000ca72742439f73777dab752ce0597
Edward Salazar <edasalazar123@gmail.com>
2023-05-13 09:19:45 -0700

    Time: 750 ms (66.67%), Space: 34.9 MB (33.33%) - LeetHub

---
+++ 2679-sum-in-a-matrix/2679-sum-in-a-matrix.py
@@ -0,0 +1,15 @@
+class Solution:
+    def matrixSum(self, nums: List[List[int]]) -> int:
+        highest_scores = []
+        ans = 0
+        nums = [sorted(num) for num in nums]
+
+        while any(nums):
+            temp = 0
+            for num in nums:
+                temp = max(temp, num.pop())
+            ans += temp 
+        
+
+               
+        return ans
