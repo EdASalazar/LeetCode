ae22c783f1abfe8b41dc2c95c66e2fa81c306569
Edward Salazar <edasalazar123@gmail.com>
2023-05-19 17:51:53 -0700

    Time: 170 ms, Memory: 21.6 MB - LeetHub

---
+++ word-ladder/word-ladder.py
@@ -0,0 +1,34 @@
+from collections import deque, defaultdict
+
+
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if endWord not in wordList: 
+            return 0
+
+        adjacent = defaultdict(list)
+
+        for word in wordList:
+            for i in range(len(word)):
+                adjacent[word[:i] + '*' + word[i+1:]].append(word)
+            # print(adjacent)
+        
+        visited = set()
+        queue = deque([(beginWord, 1)])
+
+        while queue:
+            word, steps = queue.popleft()
+            if word == endWord:
+                return steps
+            if word not in visited:
+                visited.add(word)
+                for i in range(len(word)):
+                    neighbors = word[:i] + '*' + word[i+1:]
+                    for neighbor in adjacent[neighbors]:
+                        queue.append((neighbor, steps + 1))
+        return 0
+                            
+            
+        
+        
+        
