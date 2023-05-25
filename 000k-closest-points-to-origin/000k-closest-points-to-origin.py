432e76d71a7108a415903d3727fe34df75796fba
Edward Salazar <edasalazar123@gmail.com>
2023-05-24 18:03:58 -0700

    Time: 853 ms, Memory: 22.7 MB - LeetHub

--- 000k-closest-points-to-origin/000k-closest-points-to-origin.py
+++ 000k-closest-points-to-origin/000k-closest-points-to-origin.py
@@ -9,11 +9,9 @@ class Solution:
             pointOne = point[0]
             pointTwo = point[1]
             distance = pointOne**2 + pointTwo**2
-            print(pointOne, pointTwo, distance)
             heapq.heappush(heap, (distance, (pointOne, pointTwo)))
          
         
-        print(heap)
         
         return [heappop(heap)[1] for _ in range(k)]
         
