d5afbcbe3bca905da5ef3f1dd606dbba0852aedd
Edward Salazar <edasalazar123@gmail.com>
2023-06-02 19:33:20 -0700

    Time: 75 ms, Memory: 16.8 MB - LeetHub

--- numbers-with-same-consecutive-differences/numbers-with-same-consecutive-differences.py
+++ numbers-with-same-consecutive-differences/numbers-with-same-consecutive-differences.py
@@ -1,29 +1,21 @@
 class Solution:
-    def numsSameConsecDiff(self, n: int, k: int) -> List[int]:
-        
-        
+    def numsSameConsecDiff(self, n: int, k: int) -> List[int]: 
         
         def backtrack(digit):
             if len(digit) == n:
-                print('backtrack')
                 num = (int(''.join(map(str, digit))))
-                str(num)
                 if len(str(num)) == n:
                     ans.append(num)
                 return
             
-            for i in range(0, 10):
+            for i in range(10):
                 if digit == []:
-                    print('if 1')
                     digit.append(i)
-                    print('if 1', digit)
                     backtrack(digit)
                     digit.pop()
                 else:
                     if abs(i - digit[-1]) == k:
-                        print('if 2')
-                        digit.append(i)
-                        print('if 2', digit)
+                        digit.append(i)                   
                         backtrack(digit)
                         digit.pop()
                         
