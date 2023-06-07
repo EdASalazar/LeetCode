32bdf86d9bc92280bd5c8361470dc23b6623cd53
Edward Salazar <edasalazar123@gmail.com>
2023-06-07 16:04:01 -0700

    Time: 64 ms, Memory: 16.4 MB - LeetHub

--- unique-paths-ii/unique-paths-ii.py
+++ unique-paths-ii/unique-paths-ii.py
@@ -3,25 +3,18 @@ class Solution:
         m = len(obstacleGrid)
         n = len(obstacleGrid[0])
         
-        if obstacleGrid[m - 1][n -1] == 1: 
+        dp = [[0] * n for _ in range(m)]
+        dp[0][0] = 1
+        
+        if obstacleGrid[m - 1][n - 1] == 1:
             return 0
-    
-        @cache
-        def dp(row, col):
-            if row + col == 0:
-                return 1
-            ways = 0
-            
-            if row > 0 and obstacleGrid[row -1][col] < 1:
-                ways += dp(row -1, col)
-            if col > 0 and obstacleGrid[row][col -1] < 1:
-                ways += dp(row, col -1)
-            return ways
         
-        
-        
-
-        
-        
-        return dp(m - 1, n -1)
+        for row in range(m):
+            for col in range(n):
+                if row > 0 and obstacleGrid[row -1][col] < 1:
+                    dp[row][col] += dp[row -1][col]
+                if col > 0 and obstacleGrid[row][col -1] < 1:
+                    dp[row][col] += dp[row][col -1]
+                    
+        return dp[m - 1][n - 1]
         
